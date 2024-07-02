d5d7bdb9a73ee5e46555bc33b119695b539c0974
HandsomeSquidward <agadrian1331@gmail.com>
2024-07-02 15:56:14 -0400

    Initial commit

---
+++ src/main/java/tofu/mods/WhereIsMySpawn.java
@@ -0,0 +1,18 @@
+package tofu.mods;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tofu.mods.command.FindHomeCommand;
+
+public class WhereIsMySpawn implements ModInitializer {
+	public static final String MOD_ID = "where-is-my-spawn";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register(FindHomeCommand::register);
+	}
+}
